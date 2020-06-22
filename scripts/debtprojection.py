b920869db9e0b75e192a3526624e5206f103488b
Angelica <Angelica137@users.noreply.github.com>
2020-06-22 23:18:45 +0100

    add next months balance

--- scripts/debtprojection.py
+++ scripts/debtprojection.py
@@ -1,18 +1,5 @@
 from scripts.minmonthpymt import MinMonthPymt
-
-
-class Statement:
-    def __init__(self, balance, minMonthPymtRate):
-        self._balance = balance
-        self._minMonthPymtRate = minMonthPymtRate
-        self._minPmtThisMonth = 0
-
-    def getMinPmt(self, balance, minMonthPymtRate):
-        minPmtThisnMonth = MinMonthPymt(balance, minMonthPymtRate)
-        return minPmtThisnMonth.calcMinMonPymt(minMonthPymtRate, balance)
-
-    def printStatement(self, balance, minPmtThisMonth):
-        return "Balance: " + str(balance) + "; Minimum Payment: " + str(minPmtThisMonth)
+from scripts.monthintrate import MonthlyIntRate
 
 
 class DebtProjection:
@@ -20,14 +7,37 @@ class DebtProjection:
         self._balance = balance
         self._minMonthPymtRate = minMonthPymtRate
         self._minPmtThisMonth = 0
+        self._unpaidBalance = 0
+        self._annualInt = 0.2
+        self._monthlyInt = 0
 
     def getMinMonPmt(self, balance, minMonthPymtRate):
-        minPmtThisnMonth = MinMonthPymt(balance, minMonthPymtRate)
-        self._minPmtThisMonth = minPmtThisnMonth.calcMinMonPymt(
+        minPmt = MinMonthPymt(balance, minMonthPymtRate)
+        self._minPmtThisMonth = minPmt.calcMinMonPymt(
             minMonthPymtRate, balance)
         return self._minPmtThisMonth
 
     def getMonUnpaidBalance(self, balance, minPmtThisMonth):
-        return self._balance - self._minPmtThisMonth
+        self._unpaidBalance = self._balance - self._minPmtThisMonth
+        return self._unpaidBalance
 
-    # def getNextMonthBalance():
+    def monthlyIntRate(self, annualInt):
+        monthlyInt = MonthlyIntRate(annualInt)
+        self._monthlyInt = monthlyInt.calcMonthRate()
+        return monthlyInt
+
+    def getNextMonthBalance(self, unpaidBlalance, monthlyInt):
+        self._balance = self._unpaidBalance + \
+            (self._monthlyInt * self._unpaidBalance)
+        return round(self._balance, 2)
+
+        # def TwelveMonthProjection()
+
+
+projection1 = DebtProjection(42, 0.04)
+print(projection1._balance)
+print(projection1.getMinMonPmt(42, 0.04))
+print(projection1.getMonUnpaidBalance(42, 1.68))
+print(projection1._unpaidBalance)
+print(projection1.monthlyIntRate(0.2))
+print(projection1.getNextMonthBalance(40.32, 0.2/12))
--- tests/test_debtprojection.py
+++ tests/test_debtprojection.py
@@ -1,16 +1,4 @@
-from scripts.debtprojection import Statement
 from scripts.debtprojection import DebtProjection
-
-
-def test_generate_monthly_statement():
-    statementMonth0 = Statement(42, 0.04)
-    assert statementMonth0.getMinPmt(42, 0.04) == 1.68
-
-
-def test_print_statement():
-    statementMonth0 = Statement(42, 0.04)
-    assert statementMonth0.printStatement(
-        42, 1.68) == "Balance: 42; Minimum Payment: 1.68"
 
 
 def test_minimum_monthly_payment():
@@ -22,3 +10,11 @@ def test_monthly_unpaid_balance():
     projection1 = DebtProjection(42, 0.04)
     projection1.getMinMonPmt(42, 0.04)
     assert projection1.getMonUnpaidBalance(42, 1.68) == 40.32
+
+
+def test_next_month_balance():
+    projection1 = DebtProjection(42, 0.04)
+    projection1.getMinMonPmt(42, 0.04)
+    projection1.getMonUnpaidBalance(42, 1.68)
+    projection1.monthlyIntRate(0.2)
+    assert projection1.getNextMonthBalance(40.32, 0.0166666) == 40.99

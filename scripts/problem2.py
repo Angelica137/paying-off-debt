c6b2b35e0e4ccd83ab5c5c107dd07f0f8829d430
Angelica <Angelica137@users.noreply.github.com>
2020-06-23 21:10:00 +0100

    answer problem 2 check and guess algo

---
+++ scripts/problem2.py
@@ -0,0 +1,20 @@
+balance = 3329
+annualInterestRate = 0.2
+
+monthlyInterestRate = annualInterestRate/12
+monthlyPayment = 0
+outsBalance = balance
+while outsBalance > 0:
+    monthlyPayment += 10
+    outsBalance = balance
+    month = 1
+
+    while month <= 12 and outsBalance > 0:
+        monthUnpaidBalance = outsBalance - monthlyPayment
+        updatedBalance = monthUnpaidBalance + \
+            (monthlyInterestRate * monthUnpaidBalance)
+        outsBalance = updatedBalance
+        month += 1
+    outsBalance = round(outsBalance, 2)
+
+print(monthlyPayment)
